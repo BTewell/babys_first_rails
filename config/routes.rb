e644102368f5b2f1eec80ae643ed63e284d22d58
“Brett <bretttewell@gmail.com>
2019-03-07 12:55:09 -0600

    babys_first_rails initial commit, add controller, welcome and about route, json views.

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    get '/hello' => 'welcomes#hello'
+    get '/about' => 'welcomes#about'
+  end
+end
